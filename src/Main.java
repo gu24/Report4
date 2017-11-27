43aa3732814f8a286501398fd50e313e77160332
Ryoma TAGUCHI <e175724@ie.u-ryukyu.ac.jp>
2017-11-28 05:11:55 +0900

    Report4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String str = null;
-        str.length();
+        System.out.println(str.length());
     }
 }
