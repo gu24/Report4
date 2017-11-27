4f1423c97d6f3ed09f35b56906728410aded4bfb
Ryoma TAGUCHI <e175724@ie.u-ryukyu.ac.jp>
2017-11-28 05:16:29 +0900

    Report4

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         try{
             String str = null;
-            str.length();
+            System.out.println(str.length());
         }catch (NullPointerException e){
             System.out.println("NullPointerExceptionが発生");
             System.out.println(e.getMessage());
